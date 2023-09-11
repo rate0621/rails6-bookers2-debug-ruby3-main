2f93effb4a4eef3b2496a77dabde21aa9780d482
EC2 Default User <ec2-user@ip-172-31-33-175.ap-northeast-1.compute.internal>
2023-09-11 12:23:39 +0000

    吹き飛んで通らなくなったテストコードの部分を通るように修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,20 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :show, :new, :create, :edit, :update, :destroy]  # 余計なアクションがあるかも
+
   before_action :ensure_correct_user, only: [:edit, :update]
 
   def new
     @book = Book.new
   end
 
-  def edit
+  def update
     @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+      redirect_to @book, notice: 'Book was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def show
@@ -14,7 +22,31 @@ class BooksController < ApplicationController
     @user = @book.user
   end
 
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
 
+  def create
+    @book = Book.new(book_params)
+    @book.user = current_user # ログイン中のユーザーをbookのuserとして設定
+
+    if @book.save
+      @new_book = Book.new
+      redirect_to @book, notice: 'Book was successfully created.'
+    else
+      # エラーメッセージを表示して、ユーザーに問題を修正する機会を与えます。
+      # indexアクションの@books変数も設定する必要があります
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path, notice: 'Book was successfully destroyed.'
+  end
 
 
   private
@@ -24,6 +56,11 @@ class BooksController < ApplicationController
     unless @book.user_id == current_user.id
       redirect_to books_path, alert: "You don't have permission to edit this book."
     end
-end
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 
 end
